afad23a7303f8e104db56fdaae0be1d5a661e3c7
Folka2134 <folka2134@gmail.com>
2022-10-12 15:41:50 +0100

    root directory provides ejs file with dummy data

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const app = express();
 const PORT = 4000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(artists);
+  const data = artists;
+  res.render("index.ejs", { info: data });
 });
 
 app.listen(PORT, () => {
