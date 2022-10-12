e4917c992fb4cf91ccca9deca6c057475823e9f5
Folka2134 <folka2134@gmail.com>
2022-10-12 15:05:39 +0100

    created local dummy data

---
+++ artists.js
@@ -0,0 +1,28 @@
+const artist = [
+  {
+    alias: "Chance the Rapper",
+    name: "Chancelor Bennett",
+    age: "29",
+    genre: ["hiphop", "R&B"],
+  },
+  {
+    alias: "Tyler the Creator",
+    name: "Tyler Gregory Okonma",
+    age: "31",
+    genre: ["hiphop", "jazz", "rap", "R&B", "neo", "soul", "horrorcore"],
+  },
+  {
+    alias: "Denzel Curry",
+    name: "Denzel Rae Don Curry",
+    age: "27",
+    genre: [
+      "hiphop",
+      "trap",
+      "punk-rap",
+      "hardcore hiphop",
+      "industrial hiphop",
+    ],
+  },
+];
+
+module.exports = { artist };
