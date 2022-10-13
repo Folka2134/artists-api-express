fc9d08eb61948bf3787ce00f922de9cac0f82df2
Folka2134 <folka2134@gmail.com>
2022-10-13 12:27:02 +0100

    Created env file and connected to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { artists } = require("./artists");
+require("dotenv").config();
 
 const app = express();
 const PORT = 4000;
 
+// MONGODB STUFF
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+// Middleware
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 
+// Routes
 app.get("/", (req, res) => {
   const data = artists;
   res.render("index.ejs", { info: data });
