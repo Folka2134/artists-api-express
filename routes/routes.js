667166e7fd7ba23c9ec190f52ca7b26748df0556
Folka2134 <folka2134@gmail.com>
2022-10-13 13:08:23 +0100

    posting data to database

--- routes/routes.js
+++ routes/routes.js
@@ -10,4 +10,20 @@ router.get("/getAll", (req, res) => {
   res.send("Get All API");
 });
 
+// POST route
+router.post("/post", async (req, res) => {
+  const artist = new Model({
+    alias: req.body.alias,
+    name: req.body.name,
+    likes: 0,
+  });
+
+  try {
+    const saveArtist = await artist.save();
+    res.status(200).json(saveArtist);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 module.exports = router;
