0443f0f0c0257803da27ab1ec071f43a6e15bdc6
Folka2134 <folka2134@gmail.com>
2022-10-14 13:07:58 +0100

    added like functionality on client-side

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,12 @@
 const deleteText = document.querySelectorAll(".delete");
-const likeText = document.querySelectorAll(".like");
+const likeText = document.querySelectorAll(".likes");
 
 deleteText.forEach((element) => {
   element.addEventListener("click", deleteArtist);
+});
+
+likeText.forEach((element) => {
+  element.addEventListener("click", addLike);
 });
 
 async function deleteArtist() {
@@ -17,6 +21,29 @@ async function deleteArtist() {
         name: name,
       }),
     });
+    location.reload();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addLike() {
+  const alias = this.parentNode.childNodes[1].innerText;
+  const name = this.parentNode.childNodes[3].innerText;
+  const likes = Number(this.parentNode.childNodes[5].innerText);
+
+  try {
+    const reponse = await fetch("api/addLike", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        alias: alias,
+        name: name,
+        likes: likes,
+      }),
+    });
+    const data = await reponse.json();
+    console.log(data);
     location.reload();
   } catch (error) {
     console.log(error);
